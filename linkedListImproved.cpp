ffc31fc1b578108d666d61d82069d0c727a57622
Harsh Vardhan Guleria <harshvardhanguleria@gmail.com>
2021-02-15 23:45:31 +0530

    Modified

--- linkedListImproved.cpp
+++ linkedListImproved.cpp
@@ -10,17 +10,22 @@ class node {
 
 class linkedList : private node {
     node *link, *head;
+    int count;
     public:
         linkedList() {
             head = NULL;
             link = NULL;
+            count = 1;
         }
-        void create(int);
+        int getCount() {
+            return count;
+        }
+        void insert(int, int);
         void display();
 };
 
 int main() {
-    int d, choice;
+    int d, choice, c;
     char ch = 'y';
     linkedList l;
     while (ch == 'y') {
@@ -31,9 +36,21 @@ int main() {
         cin >> choice;
         switch(choice) {
             case 1:
-                cout << "Enter the integer to insert: ";
+                cout << endl << "Enter the integer to insert: ";
                 cin >> d;
-                l.create(d);
+                cout << endl << "1. At the beginning";
+                cout << endl << "2. At any position";
+                cout << endl << "3. At the end";
+                cout << endl << "The list has " << l.getCount() - 1
+                    << " elements";
+                cout << endl << "Enter your choice: ";
+                cin >> c;
+                if (c == 1) 
+                    l.insert(d, 1);
+                else if (c == 2)
+                    l.insert(d, 2);
+                else
+                    l.insert(d, 3);
                 break;
             case 2:
                 cout << endl << "The list is";
@@ -49,17 +66,37 @@ int main() {
     return 0;
 }
 
-void linkedList::create(int data) {
+void linkedList::insert(int data, int position) {
+    count = count + 1;
+    int n = 0;
     node *temp = new node;
-    temp->data = data;
-    temp->next = NULL;
-    if (head == NULL) {
-        head = temp;
-        link = temp;
-    } else {
+    if (position == 1) {
+        temp->data = data;
+        temp->next = NULL;
+        if (head == NULL) {
+            head = temp;
+            link = temp;
+        } else {
+            link->next = temp;
+            link = temp;
+        }
+    } else if (position == 2) {
+        temp->data = data;
+        temp->next = NULL;
+        while (n < position) {
+            link = link->next;
+            n = n + 1;
+        }
+        temp->next = link->next;
         link->next = temp;
-        link = temp;
-    }    
+    } else if (position == 3) {
+        temp->data = data;
+        temp->next = NULL;
+        while (link->next->next != NULL)
+            link = link->next;
+        temp->next = link->next->next;
+        link->next = temp;
+    }     
 }
 
 void linkedList::display() {
