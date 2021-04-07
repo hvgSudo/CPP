742a061fc65ce73e8d0705093087627caed53ec6
Harsh Vardhan Guleria <harshvardhanguleria@gmail.com>
2021-04-07 10:46:43 +0530

    Modified

--- BinaryTree.cpp
+++ BinaryTree.cpp
@@ -22,15 +22,32 @@ class BT {
         Node preorder(Node *current);
         Node inorder(Node *current);
         Node postorder(Node *current);
-        Node preorderLoop(Node *current);
-        Node inorderLoop(Node *current);
-        Node postorderLoop(Node *current);
+        void preorderLoop(Node *current);
+        void inorderLoop(Node *current);
+        void postorderLoop(Node *current);
     public :
         void create();
         void insert();
         void preorder();
         void inorder();
         void postorder();
+};
+
+class stack {
+    private :
+        int top;
+        Node *data[10];
+    public :
+        stack() {
+            top = -1;
+        }
+        void push(Node *p) {
+            data[++top] = p;
+        }
+        Node *pop() { return data[top--]; }
+        Node *peek() { return data[top]; }
+        int isFull() { return top == 10; }
+        int isEmpty() { return top == -1; }
 };
 
 int main() {
@@ -108,4 +125,94 @@ void BT::insert() {
         cout << "\nDo you want to insert more elements(y/n): ";
         cin >> ch;
     }while(ch == 'y' || ch == 'Y');            
+}
+
+void BT::preorder() {
+    int choice;
+    cout << endl << "1. Recursive\n" <<
+        "2. Non recursive";
+    cout << "\nEnter your choice: ";
+    cin >> choice;
+    cout << "\nPreorder traversal: ";
+    if (choice == 1) 
+        preorder(root);
+    else if (choice == 2)
+        preorderLoop(root);
+}
+
+void BT::inorder() {
+    int choice;
+    cout << endl << "1. Recursive\n" <<
+        "2. Non recursive";
+    cout << "\nEnter your choice: ";
+    cin >> choice;
+    cout << "\nPreorder traversal: ";
+    if (choice == 1) 
+        inorder(root);
+    else if (choice == 2)
+        inorderLoop(root);
+}
+
+void BT::postorder() {
+    int choice;
+    cout << endl << "1. Recursive\n" <<
+        "2. Non recursive";
+    cout << "\nEnter your choice: ";
+    cin >> choice;
+    cout << "\nPreorder traversal: ";
+    if (choice == 1) 
+        postorder(root);
+    else if (choice == 2)
+        postorderLoop(root);
+}
+
+void BT::preorderLoop(Node *current) {
+    stack s;
+    Node *temp;
+    if (current == NULL) 
+        return;
+    s.push(current);
+    while (!s.isEmpty()) {
+        temp = s.peek();
+        cout << temp->data << " ";
+        s.pop();
+        if (temp->right)
+            s.push(temp->right);
+        if (temp->left)
+            s.push(temp->left);
+    }
+}
+
+void BT::inorderLoop(Node *current) {
+    stack s;
+    Node *temp = current;
+    bool exit = false;
+    while (!exit) {
+        while (temp != NULL) {
+            s.push(temp);
+            temp = temp->left;
+        } 
+        if (s.isEmpty()) {
+            exit = true;
+            break;
+        }
+        temp = s.pop();
+        cout << temp->data << " ";
+        temp = temp->right;
+    }
+}
+
+void BT::postorderLoop(Node *current) {
+    if (current == NULL) 
+        return;
+    stack s;
+    do {
+        while (current != NULL) {
+            if (current->right)
+                s.push(current->right);
+            s.push(current);
+            current = current->left;
+        }
+        current = s.pop();
+    } while (!s.isEmpty());
 }
